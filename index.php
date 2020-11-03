5d648fa545d28acbea2920f41883f003eb5bf4b0
saci zakaria <sacideveloppeur@gmail.com>
2020-11-03 10:53:05 +0100

    require git/views/accueil.views.php; Fatal error: require(): Failed opening required 'biblio/views/accueil.views.php' (include_path='C:\xampp\php\PEAR') in C:\xampp\htdocs\biblio\index.php on line 4

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    require "biblio/views/accueil.views.php";
+    require "git/views/accueil.views.php";
 //Fatal error: require(): Failed opening required 'biblio/views/accueil.views.php' (include_path='
 //C:\xampp\php\PEAR') in C:\xampp\htdocs\biblio\index.php on line 4
 
